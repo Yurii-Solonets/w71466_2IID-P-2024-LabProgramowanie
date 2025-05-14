75f42362b31d24834b61f62c840cd6b7fbe8fcec
student71466 <urijsolonec@gmail.com>
2025-05-14 11:05:00 +0200

    Laboratorium 6

--- Laboratorium 6/script.js
+++ Laboratorium 6/script.js
@@ -1,10 +1,54 @@
 // <<--Zadanie 1 -->> 
+function silnia(n){
+    return n > 1? n * silnia(n - 1): 1;
+}
+// const m = prompt("Podaj liczbę dla silnii: ");
+console.log(silnia(5));
 
-const silna = prompt ("Podaj siłne przez którą: ")
-const liczba = prompt("Podaj liczbę: ")
+// <<--Zadanie 2 -->>
+const btns = document.querySelectorAll("button");
+let selectedBtn = null;
+btns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        btn.classList.add("selected");
+        if(selectedBtn){
+            selectedBtn.classList.remove("selected")   
+        }
+        selectedBtn = btn;
+    })
+})
 
-function mnożenie(){
-    return silna ** liczba
+// <<--Zadanie 3 -->>
+const spanToHide = document.querySelector("span")
+const btn = document.querySelector("#secretBtn")
+
+btn.addEventListener("click", toogleVisibility);
+
+function toogleVisibility(){
+    spanToHide.style.visibility = spanToHide.style.visibility === 'hidden'? "visible" : "hidden";
 }
 
-console.log(mnożenie(silna, liczba))
+// <<-- Zadanie 4 -->>
+const nameInput = document.querySelector("#name");
+const surnameInput = document.querySelector("#surname")
+const saveBtn = document.querySelector("#save")
+const list = document.querySelector(".list")
+const table = document.querySelector("table")
+
+
+saveBtn.addEventListener("click", () => {
+    const name = nameInput.value;
+
+    const li = document.createElement("li");
+    li.textContent = name;
+    list.appendChild(li)
+
+    // <<-- Zadanie 5 -->>
+    const surname = surnameInput.value;
+    const newRow = table.insertRow();
+    const nameCall = newRow.insertCell();
+    nameCall.textContent = name;
+    const surnameCall = newRow.insertCell();
+    surnameCall.textContent = surname;
+    li.textContent = name + " " + surname
+})  
