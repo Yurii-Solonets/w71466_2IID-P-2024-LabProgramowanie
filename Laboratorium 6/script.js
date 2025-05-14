59fd494799ce12cc31d928ef6b580cc0727052a5
student71466 <urijsolonec@gmail.com>
2025-05-14 10:06:13 +0200

    Laboratorium 6

---
+++ Laboratorium 6/script.js
@@ -0,0 +1,10 @@
+// <<--Zadanie 1 -->> 
+
+const silna = prompt ("Podaj siłne przez którą: ")
+const liczba = prompt("Podaj liczbę: ")
+
+function mnożenie(){
+    return silna ** liczba
+}
+
+console.log(mnożenie(silna, liczba))
