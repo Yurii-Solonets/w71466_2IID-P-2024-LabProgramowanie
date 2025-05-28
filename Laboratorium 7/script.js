23d30ccc94abb587c8894c7fb81893b96a84df05
student71466 <urijsolonec@gmail.com>
2025-05-28 11:22:52 +0200

    Laboratorium 7

---
+++ Laboratorium 7/script.js
@@ -0,0 +1,49 @@
+const form = document.querySelector('form');
+
+//Zadanie 2
+const nameInput = document.querySelector("#name")
+const nameDisplay = document.querySelector("#nameDisplay")
+nameInput.addEventListener('input', () => {
+    nameDisplay.textContent = nameInput.value;
+})
+
+//Zadanie 3
+const showHidePassword = document.querySelector("#showHidePassword")
+const passwordInput = document.querySelector("#password")
+
+showHidePassword.addEventListener("change", () =>{
+   passwordInput.type = showHidePassword.checked? "text": "password";
+})
+
+
+//Zadanie
+const hobbiesList = document.querySelector('ul');
+function hobbies(){
+    hobbiesList.replaceChild()
+    const selectedHobbies = document.querySelectorAll('input[name = "hobbies"]:checked')
+
+    selectedHobbies.forEach(hobby => {
+        const item = document.createElement('li')
+        item.textContent = hobby.value;
+        hobbiesList = appendChild(item)
+    })
+}
+
+form.addEventListener("submit", event => {
+    event.preventDefaul();
+
+    hobbies();
+})
+
+//Zadanie 5
+
+const phoneContainer = document.querySelector('#phone-container')
+const showHidePhone = document.querySelector("#showHidePhone")
+showHidePhone.addEventListener("change", () => {
+    phoneContainer.hidden = showHidePhone.checked;
+})
+
+//Zadanie 6 
+const countrySelect = document.querySelector("#country")
+const franceOption = new Option("Francja", "FR");
+countrySelect.add(franceOption)
